8dd49e1bfd61f2561a003c542e16d99165c73893
mohitsingh <iammohitsingh109@gmail.com>
2026-05-11 10:14:35 +0530

    function

---
+++ function/product.py
@@ -0,0 +1,14 @@
+# User input product id
+# User input product name
+# User input product price
+# User input product quantity
+
+# product_input() -> multiple value return
+# display_product_info(id,product_name,price,quantity) print (we only want to print the info if validation pass)
+# validate_product_info(id,product_name,price,quantity) True, "Success"
+# False, "Error message"
+# "Error message" --> product name less than 3 char
+# "Error message" --> quantity is 0 or negative
+# "Error message" --> price can't be 0 or less
+# "Error message" -> id should not be empty
+# keep asking user until you make it pass all the cases
