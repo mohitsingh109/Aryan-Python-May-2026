da018e0b4725c7dff4dc163a9e2f95ffbf496d1c
aryan210602-netizen <aryan210602@gmail.com>
2026-05-19 18:51:01 -0700

    Updated password validation

--- function/product.py
+++ function/product.py
@@ -12,3 +12,38 @@
 # "Error message" --> price can't be 0 or less
 # "Error message" -> id should not be empty
 # keep asking user until you make it pass all the cases
+# product description , add validation where product description should not be less than 5 characters , if validation pass and display
+
+def product_input():
+    product_id = input("Enter product id:")
+    product_name = input("Enter product name:")
+    product_price = float(input("Enter product price:"))
+    product_quantity = int(input("Enter product quantity:"))
+    product_description=input("Enter product description:")
+    return product_id, product_name , product_price , product_quantity, product_description
+
+def display_product_info(product_id,product_name,product_price,product_quantity,product_description_1):
+    print(f" product id : {product_id} , product name : {product_name} , product price : {product_price} , product quantity:{product_quantity} ,product description: {product_description_1} ")
+
+def validate_product_input(product_id, product_name,product_price,product_quantity,product_description):
+    if len(product_name) <= 3:
+        return False , "product name is too short"
+    if product_quantity <= 0:
+        return False, " product quantity should be greater than 0"
+    if product_price <= 0:
+        return False, "product price should be greate than 0"
+    if product_id == "":
+        return False , "product id is empty"
+    if len(product_description) <= 5:
+        return False, "product description is too short"
+    return True , "product is valid"
+
+while True:
+    product_id, product_name, product_price, product_quantity, product_description = product_input()
+    state, message = validate_product_input(product_id, product_name, product_price, product_quantity, product_description)
+    if state == True:
+        display_product_info(product_id, product_name, product_price, product_quantity, product_description)
+        break
+    else:
+        print(f" validation failed due to: {message}")
+
