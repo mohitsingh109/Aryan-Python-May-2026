30d0573aa862e92b792093b3bf272bc1c0cb7e75
aryan210602-netizen <aryan210602@gmail.com>
2026-05-19 18:51:16 -0700

    Updated password validation

--- function/course.py
+++ function/course.py
@@ -18,8 +18,8 @@
 # amount (if is_paid = true)
 
 # validation ==> not empty for (id, name, description), amount should be > 0 if is_paid = true
-#============#============#============#============#============#============#============#============
 # infinite loop here until user provide correct value
+#============#============#============#============#============#============#============#============
 # 2. edit a course
 # name
 # description
@@ -34,6 +34,97 @@
         # ==> validate
         # if validate pass break
         # else ask user again
+c_id = None
+name = None
+description = None
+paid = None
+amount = None
+
+def add_a_course():
+    while True:
+        global c_id, name, description, paid, amount
+        c_id, name, description ,paid, amount = course_input ()
+        state, message = validate_course_input()
+        if state:
+            display_course_details()
+            break
+        else :
+            print(f"{message}")
+
+
+
+def course_input():
+    c_id = input("Enter a course id:")
+    name = input("Enter a course name:")
+    description = input("enter a course description :")
+    paid = input("Enter a course is paid or free:")
+    if paid == "paid":
+        amount = int(input("enter a course amount:"))
+    else:
+        amount = 0
+    return c_id, name, description, paid , amount
+
+def display_course_details():
+    global c_id, name, description, paid, amount
+    print(f'Course id :{c_id}", name : {name}, desciption :{description}, paid :{paid}, amount : {amount}')
+
+def validate_course_input():
+    global c_id, name, description, paid, amount
+    if len(c_id) > 6 or len(c_id) < 1:
+        return False ,"Course id is too long or too short"
+    if len(name) > 15 or len(name) < 5:
+        return False ,"Course name is too long or too short "
+    if len(description) > 15 or len(description) < 5 :
+        return False, "Course description is too long or too short"
+    if amount <= 0 and paid == "paid" :
+        return False, "Course amount cannot be less than 0"
+    return True, "Success"
+
+
+def edit_course_details():
+    global c_id, name, description, paid, amount
+    c_id_edit,name_edit, description_edit, paid_edit, amount_edit = course_input()
+
+    if c_id_edit != c_id or c_id is None:
+        print("Course id does not match")
+        return
+
+    if c_id_edit != "" and c_id_edit is not None:
+         c_id = c_id_edit
+
+    if name_edit != "" and name_edit is not None:
+        name = name_edit
+
+    if description_edit != "" and description_edit is not None:
+        description = description_edit
+
+    if paid_edit != "" and paid_edit is not None:
+        paid = paid_edit
+
+    if amount_edit is not None and amount_edit == 0:
+        amount = amount_edit
+
+    display_course_details()
+
+def delete_course():
+    global c_id, name, description, paid, amount
+    c_id_delete = input("Enter a course id to be deleted:")
+    if c_id_delete != c_id:
+        print("Course id does not match")
+        return
+    c_id = None
+    name = None
+    description = None
+    paid = None
+    amount = None
+
+
+
+
+
+
+
+
 
 while True:
     print("\n------------------------------------------------------------\n")
@@ -47,13 +138,13 @@ while True:
 
     choice = int(input("Enter your choice: "))
     if choice == 1:
-        print("Add new course") # you will call a function
+        add_a_course()
     elif choice == 2:
-        print("Edit a course")
+        edit_course_details()
     elif choice == 3:
-        print("Delete a course")
+        delete_course()
     elif choice == 4:
-        print("View a course")
+        display_course_details()
     elif choice == 5:
         print("Quit")
         break
